06331fccc00d282a68ff40e1436ac1ba5ad03156
Shubham Singh <shubhams.167@gmail.com>
2018-08-19 05:19:15 +0530

    Minor fixes
    Readability of code has been improved

--- 1. myFirstCudaProject/myFirstCudaProject/kernel.cu
+++ 1. myFirstCudaProject/myFirstCudaProject/kernel.cu
@@ -4,7 +4,7 @@
 *	This program basically performs two tasks:
 *	1. It selects suitable CUDA enabled device(GPU) and prints the device properties
 *	2. It demonstrate basic parallel addition of two arrays on the device(GPU) using add kernel.
-*	@Author: Shubham Singh
+*	Author: Shubham Singh
 **/
 
 #include "cuda_runtime.h"
@@ -32,6 +32,7 @@ int main()
 	int count, dev;	
 	cudaGetDevice(&dev);			/*Function to get current device ID and store device ID in dev*/
 	cout << "ID of current cuda Device: " << dev<< endl;
+	
 	/*
 	*	If system has multiple GPUs then
 	*	Find CUDA device(GPU) having major computing capability greater than 1
@@ -43,6 +44,7 @@ int main()
 	cudaChooseDevice(&dev, &prop);	/*Get device ID for revision greater than 1.3*/
 	cout << "ID of CUDA device closest to revision 1.3: " << dev << endl;
 	cudaSetDevice(dev);				/*Set current device(GPU) to device ID dev which is having revision greater than 1.3*/
+	
 	/*
 	*	Get properties of device dev from CUDA runtime and hold properties into prop
 	*	and print few details of device dev
@@ -67,6 +69,7 @@ int main()
 	cout << "Max grid dimension: " << prop.maxGridSize[0] << " " << prop.maxGridSize[1] << " " << prop.maxGridSize[2] << " " << endl;
 	cout << "Size of L2 cache(in bytes): " << prop.l2CacheSize << endl;
 	cout << "Device's revision/compute capability: " << prop.major << "." << prop.minor << endl;
+	
 	/*
 	*	So, we are done with printing device(GPU) details
 	*	It's time to perform some parallel computation on device(GPU)
@@ -76,16 +79,19 @@ int main()
 	cout << "\nLet's perform some addition on arrays" << endl;
 	int *a, *b, *c;					/*Variables to hold arrays on host(CPU)*/
 	int *dev_a, *dev_b, *dev_c;		/*Variables to hold arrays on device(GPU)*/
+
 	/*Allocate memory for arrays on host(CPU)*/
 	a = new int[N];
 	b = new int[N];
 	c = new int[N];
+
 	/*Fill values in arrays on host(CPU)*/
 	for (int i = 0; i < N; i++)
 	{
 		a[i] = i;
 		b[i] = N - i - 1;
 	}
+
 	/*Print arrays*/
 	cout << "Array a: ";
 	for (int i = 0; i < N; i++)
@@ -93,29 +99,37 @@ int main()
 	cout << "\nArray b: ";
 	for (int i = 0; i < N; i++)
 		cout << b[i] << " ";
+
 	/*Allocate memory for arrays on device(GPU)*/
 	cudaMalloc((void **)&dev_a, N * sizeof(int));
 	cudaMalloc((void **)&dev_b, N * sizeof(int));
 	cudaMalloc((void **)&dev_c, N * sizeof(int));
+
 	/*Copy arrays a and b from host(CPU) to device(GPU)*/
 	cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
 	/*Call add kernel to perform addition on device(GPU)*/
 	add <<< N, 1 >>> (dev_a, dev_b, dev_c);
+
 	/*Copy c array back from device(GPU) to host(CPU)*/
 	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+
 	/*Print sum*/
-	cout << "\nSum of both arrays: ";
+	cout << "\nArray c: ";
 	for (int i = 0; i < N; i++)
 		cout << c[i] << " ";
 	cout << endl;
+
 	/*Free memory allocated on host(CPU)*/
 	delete[] a;
 	delete[] b;
 	delete[] c;
+
 	/*Free memory allocated on device(GPU)*/
 	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
+
 	return 0;
 }
